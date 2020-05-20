fbc45b4ebd6f89df2dd56a61d571e7d2fa942c54
Ajay <ajay_sieunarine@hotmail.com>
2020-05-20 10:17:03 -0400

    fixed OOP stuff

--- src/events/CustomClient.py
+++ src/events/CustomClient.py
@@ -1,9 +1,33 @@
 import discord
+import os
+from dotenv import load_dotenv
+
+"""
+client object represents a connection to discord.
+handles events, tracks state, interacts with discord APIs
+"""
 
 
-class CustomClient(discord.Client):
+class CustomClient(discord.Client): # inherit from discord.Client
+
     def __init__(self):
-        DICT_MEMBERS = {}
+        super().__init__()
+        # get context from .env
+        load_dotenv()
+        # get values from .env
+        self.TOKEN = os.getenv('DISCORD_TOKEN')
+        self.MYGUILD = os.getenv('DISCORD_GUILD')
+        self.DICT_MEMBERS = {}
+
+    # EVENTS
+    """
+    client on_ready event handler
+    - handles the event when the Client has established a connection to Discord
+    and as finished preparing the data that discord has sent, such as:
+        - login state
+        - server/guild
+        - channel data ,etc.
+    """
 
     async def on_ready(self):
         print(f'{self.user} has connected!')
@@ -14,3 +38,33 @@ class CustomClient(discord.Client):
         await member.dm_channel.send(
             f'Hi {member.name}, welcome to my Discord server!'
         )
+
+    # METHODS
+    # ACCESSORS
+    def get_dict_members(self):
+        return self.DICT_MEMBERS
+
+    def get_token(self):
+        return self.TOKEN
+
+    def get_my_guild(self):
+        return self.MYGUILD
+
+    # UTILITY
+    def get_guilds(self):
+        print(f'{self.user} is connected to the following guilds:\n')
+
+        for guild in self.guilds:
+            if guild.name == self.MYGUILD:
+                print(f'{guild.name}(id: {guild.id})' + f' (my guild)')
+            else:
+                print(
+                    f'{guild.name}(id: {guild.id})'
+                )
+            members_values = '\n - '.join([member.name for member in guild.members])
+            self.DICT_MEMBERS[guild.name] = members_values
+            # print(f'Guild Members:\n - {members}')
+
+    def get_my_guildl(self):
+        # return discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+        return discord.utils.get(self.guilds, name=self.MYGUILD)
